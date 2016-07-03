a6db870613c59de704c710ded999a35b1177f17a
Victor <victorsants360@gmail.com>
2016-07-03 09:47:22 -0300

    Cadastro de Animais

---
+++ app/views/animals/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @animal, :id, :name, :breed, :birth, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703122459) do
+ActiveRecord::Schema.define(version: 20160703124330) do
+
+  create_table "animals", force: :cascade do |t|
+    t.string   "name"
+    t.string   "breed"
+    t.date     "birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "tittle"
